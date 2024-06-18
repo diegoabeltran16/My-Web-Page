9767f06bde052514ba558f3ab2a5635f4caceeee
diego_pzukjqi <diegobeltran1016@hotmail.com>
2024-06-18 14:39:41 -0500

    1

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,4 @@
-// js/scripts.js
-// Añade un listener al formulario de contacto para manejar el envío
-document.getElementById('contact-form').addEventListener('submit', function(event) {
-    // Previene el comportamiento predeterminado del formulario
-    event.preventDefault();
-    // Muestra una alerta indicando que el formulario fue enviado
-    alert('Formulario enviado');
+document.addEventListener('DOMContentLoaded', () => {
+    console.log('Document is ready');
+    // Add more scripts as needed
 });
